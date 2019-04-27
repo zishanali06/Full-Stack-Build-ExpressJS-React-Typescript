4febfc6685f4fb6f852b8bbaf7e57887462ab7a7
Zishan Ali <zishanali06@gmail.com>
2019-04-27 13:24:11 -0500

    got books api working

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -7,19 +7,16 @@ export default class App extends React.Component<IAppProps, IAppState> {
     constructor(props: IAppProps) {
         super(props);
 
-        this.state = { name: null };
     }
 
     async componentWillMount() {
-        let r = await fetch('/api/hello');
-        let name = await r.json();
-        this.setState({ name })
+
     }
 
     render () {
         return (
             <main className="container">
-                <h1 className="covalence-blue">Hello {this.state.name}!</h1>
+                <h1 className="covalence-blue">Hello</h1>
                 <h2></h2>
             </main>
         )
@@ -31,5 +28,4 @@ interface IAppProps {
 }
 
 interface IAppState {
-    name: string;
 }
--- src/server/routes/api/books.ts
+++ src/server/routes/api/books.ts
@@ -7,13 +7,51 @@ router.get('/:id?', async (req, res, next) => {
     let id = req.params.id;
     if(id){
         try {
-            let book = await knex('books').select('books.id as id', 'categories.name as category', 'books.title as title', 'books.author as author', 'books.price as price').join('categories', 'categories.id', '=', 'books.categoryid').where('books.id', id)
-            console.log(book);
+            let [book] = await knex('books').select('books.id as id', 'categories.name as category', 'books.title as title', 'books.author as author', 'books.price as price').join('categories', 'categories.id', '=', 'books.categoryid').where('books.id', id);
             res.json(book);
         } catch (error) {
             console.log(error);
             res.sendStatus(500);
         }
+    } else {
+        try {
+            let books = await knex('books').select('books.id as id', 'categories.name as category', 'books.title as title', 'books.author as author', 'books.price as price').join('categories', 'categories.id', '=', 'books.categoryid');
+            res.json(books);
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        }
+    }
+});
+
+router.put('/edit/:id', async (req, res, next) => {
+    let id = req.params.id;
+    try {
+        let change = await knex('books').where('books.id', id).update({ title: req.body.title, categoryid: req.body.categoryid, author: req.body.author, price: req.body.price });
+        res.json(change);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+router.post('/add', async (req, res, next) => {
+    try {
+        let data = await knex('books').insert(req.body);
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+router.delete('/delete/:id', async (req, res, next) => {
+    try {
+        let data = await knex('books').where('books.id', req.params.id).del();
+        res.json(data);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
     }
 })
 
