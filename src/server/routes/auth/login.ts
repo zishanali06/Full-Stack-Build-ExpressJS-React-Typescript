fabd17f90279285e9d1df3eab7915512412aa7aa
Zishan Ali <zishanali06@gmail.com>
2019-04-27 15:23:59 -0500

    register working but cant login

--- src/server/routes/auth/login.ts
+++ src/server/routes/auth/login.ts
@@ -8,11 +8,15 @@ const router = express.Router();
 router.post('/', passport.authenticate('local'), async (req, res, next) => {
     try {
         let token = await CreateToken({ userid: req.user.id });
+        res.json({
+            token,
+            role: req.user.role,
+            userid: req.user.id
+        })
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
     }
 })
-
 
 export default router;
--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -1,9 +1,26 @@
 import * as express from 'express';
-
-
+import knex from '../../db';
+import { HashPassword } from '../../utils/security/passwords';
+import { CreateToken } from '../../utils/security/tokens';
 
 const router = express.Router();
 
+router.post('/', async (req, res, next) => {
+    try {
+        let user = req.body;
+        user.password = HashPassword(req.body.password);
+        let [userid] = await knex('users').insert(req.body);
+        let token = await CreateToken({ userid });
+        res.json({
+            token,
+            role: 'admin',
+            userid
+        });
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    };
+});
 
 
 export default router;
